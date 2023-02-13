797c6f2f45935da498777b96493553bcbc313e5d
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-02-13 17:57:22 -0500

    Changed my array to an object in hopes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Assignment code here
 // IDK HOW TO START THIIIIS
-let parameters = ["capital letter", " lowercase letter", " numeric", " special character"];
-parameters [1] = "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z";
-parameters [2] = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",'p','q','r','s','t','u','v','w','x','y',"z";
-parameters [3] = "0",'1','2','3','4','5','6','7','8',"9";
-parameters [4] = "!",'@','#','$','%','^','&','*';
+let parameters =  {
+
+capitals: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",'p','q','r','s','t','u','v','w','x','y',"z"],
+numbers: ["0",'1','2','3','4','5','6','7','8',"9"],
+specialChars: ["!",'@','#','$','%','^','&','*'],
+
+};
 let chars = "";
 // var pass=document.getElementById("#password");
 var pass = "";
@@ -13,38 +16,38 @@ function generatePassword() {
   var passwordAlert = window.alert("Password requires capital, lowercase, numeric, and special character")
   var passwordLength = window.prompt("Input password length between 8 and 128")
   if (confirm("Do you want numbers in your password?") == true) {
-    chars += parameters[3];
+    chars += parameters.numbers;
   }
   else {
-    !parameters[3];
+    return chars;
   }
   if (confirm("Do you want capitals in your password?") == true) {
-    chars += parameters[1];
+    chars += parameters.capitals;
   }
   else {
-    !parameters[1];
+    !capitals;
   }
   if (confirm("Do you want lowercase letters in your password?") == true) {
-    chars += parameters [2];
+    chars += parameters.lowercase;
   }
   else {
-    !parameters[2];
+    !lowercase;
   }
   if (confirm("Do you want special characters in your password?") == true) {
-    chars += parameters [4];
+    chars += parameters.specialChars;
   }
   else {
-    !parameters[4];
+    !specialChars;
   }
   if (passwordLength < 8 || passwordLength >128) {
     window.alert("Please input a proper length");
   }
-  else {
-    pass += chars.substring(Math.floor(Math.random() * passwordLength));
-    }
+  // else {
+  //   pass += chars.charAt(Math.random() * passwordLength);
+  //   }
 
-  for (var i = 0; i <=passwordLength; i++) {
-    pass += chars.substring(Math.floor(Math.random() * pass.length));
+  for (var i =passwordLength; i++;) {
+    pass += parameters.charAt(Math.floor(Math.random() * passwordLength));
   }
   
   return pass;
