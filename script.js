a4049bbeaec63f2e2dcc1a29731a624dacf2f0c5
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-02-13 17:10:58 -0500

    Added qualifying prompts

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 // Assignment code here
 // IDK HOW TO START THIIIIS
 let parameters = ["capital letter", " lowercase letter", " numeric", " special character"];
-let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%&ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+parameters [1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+parameters [2] = "abcdefghijklmnopqrstuvwxyz";
+parameters [3] = "0123456789";
+parameters [4] = "!@#$%^&*()"
+let chars = "";
 var pass=document.getElementById("#password");
 var pass = "";
 
 function generatePassword() {
-  var passwordAlert = window.alert("Password requires " + (parameters))
+  var passwordAlert = window.alert("Password requires capital, lowercase, numeric, and special character")
   var passwordLength = window.prompt("Input password length between 8 and 128")
+  if (confirm("Do you want numbers in your password?") == true) {
+    chars += parameters[3];
+  }
+  else {
+    !parameters[3];
+  }
+  if (confirm("Do you want capitals in your password?") == true) {
+    chars += parameters[1];
+  }
+  else {
+    !parameters[1];
+  }
+  if (confirm("Do you want lowercase letters in your password?") == true) {
+    chars += parameters [2];
+  }
+  else {
+    !parameters[2];
+  }
+  if (confirm("Do you want special characters in your password?") == true) {
+    chars += parameters [4];
+  }
+  else {
+    !parameters[4];
+  }
   if (passwordLength < 8 || passwordLength >128) {
     window.alert("Please input a proper length");
   }
   else {
-    pass += chars.substring(Math.floor(Math.random() * passwordLength));
+    pass += chars.substring(Math.random() * passwordLength);
     }
 
   // for (var i = 8; i <=128; i++) {
